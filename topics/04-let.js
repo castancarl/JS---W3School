c5196954885d1fbfb3c11a1204cd7eb29fb0ee36
castancarl <castancarl@gmail.com>
2022-07-22 10:20:31 +0200

    CONST

--- topics/04-let.js
+++ topics/04-let.js
@@ -1,1 +1,20 @@
-/////////////////////  LET ////////////////////////////
+/////////////////////  LET , CONST ,  ////////////////////////////
+console.log('3) Redeclaring Variables  - EXAMPLE 1');
+var x = 10;
+console.log('x = ' + x);
+{
+  var x = 3;
+  console.log('x = ' + x);
+}
+console.log('x = ' + x);
+
+//
+//
+
+console.log('4) LET Hoisting  - EXAMPLE 1');
+
+carName = 'volvo';
+var carName;
+//
+busName = 'Saab';
+let busName = 'Volvo'; // --> Reference error: can't access lexical declaration 'busName' before initialization
