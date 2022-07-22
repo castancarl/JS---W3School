0036deb095604affaf240d7a920ff49d2be3305a
castancarl <castancarl@gmail.com>
2022-07-22 18:38:32 +0200

    CONST

--- topics/04-let.js
+++ topics/04-let.js
@@ -1,4 +1,4 @@
-/////////////////////  LET , CONST ,  ////////////////////////////
+/////////////////////  LET , CONST  ////////////////////////////
 console.log('3) Redeclaring Variables  - EXAMPLE 1');
 var x = 10;
 console.log('x = ' + x);
@@ -9,12 +9,43 @@ console.log('x = ' + x);
 console.log('x = ' + x);
 
 //
-//
+// EXAMPLE
 
-console.log('4) LET Hoisting  - EXAMPLE 1');
+console.log('6) Constant Objects and Arrays  - EXAMPLE 1');
+// Create an Array:
+const cars = ['Saab', 'Volvo', 'BMW'];
 
-carName = 'volvo';
-var carName;
+// Change an element:
+cars[0] = 'Toyota';
+
+// Add an element:
+cars.push('Audi');
+
+// Display the Array:
+document.getElementById('myCars').innerHTML = cars;
+console.log(cars);
+
 //
-busName = 'Saab';
-let busName = 'Volvo'; // --> Reference error: can't access lexical declaration 'busName' before initialization
+// EXAMPLE
+
+try {
+  const friends = ['Saab', 'Volvo', 'BMW'];
+  friends = ['Toyota', 'Volvo', 'Audi'];
+} catch (err) {
+  document.getElementById('myFriends').innerHTML = err;
+  console.log(err);
+}
+
+// EXAMPLE
+
+console.log('7) Assignment Operators - EXAMPLE 1');
+
+var x = 5 + 5;
+console.log(x);
+let y = '5' + 5;
+console.log(y);
+
+let z = 'Hello' + 5;
+console.log(z);
+
+document.getElementById('string-a').innerHTML = x + '<br>' + y + '<br>' + z;
