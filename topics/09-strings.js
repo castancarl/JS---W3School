be21c8220e37dd559b4752b7b43cccd9dc62ae75
castancarl <castancarl@gmail.com>
2022-07-26 15:25:10 +0200

    STRINGS METHODS

--- topics/09-strings.js
+++ topics/09-strings.js
@@ -9,4 +9,45 @@ document.getElementById('demo').innerHTML = text.length;
 let str = 'Apple, Banana, Kiwi';
 console.log(str.length);
 document.getElementById('demo-2').innerHTML = str.length + ' ' + 'string.length';
-document.getElementById('demo-2a').innerHTML = str.slice(-12, -6);
+document.getElementById('demo-2a').innerHTML = str.slice(7, 13);
+
+//DEMO-3 -----------------------------------------
+function myFunction() {
+  let text = document.getElementById('demo-3').innerHTML;
+  document.getElementById('demo-3').innerHTML = text.replace('Microsoft', 'W3Schools');
+}
+function myReFunction() {
+  let text = document.getElementById('demo-3').innerHTML;
+  document.getElementById('demo-3').innerHTML = text.replace('W3Schools', 'Microsoft');
+}
+
+//DEMO-4 -----------------------------------------
+function myFunction2() {
+  let text = document.getElementById('demo-4').innerHTML;
+  document.getElementById('demo-4').innerHTML = text.replace(/Microsoft/g, 'W3Schools');
+}
+function myReFunction2() {
+  let text = document.getElementById('demo-4').innerHTML;
+  document.getElementById('demo-4').innerHTML = text.replace('W3Schools', 'Microsoft');
+}
+
+//DEMO-5 -----------------------------------------
+function myFunA() {
+  let text = document.getElementById('demo-5').innerHTML;
+  document.getElementById('demo-5').innerHTML = text.toUpperCase();
+}
+function myFunAre() {
+  let text = document.getElementById('demo-5').innerHTML;
+  document.getElementById('demo-5').innerHTML = text.toLowerCase();
+}
+
+//DEMO-6 -----------------------------------------
+{
+  let text1 = 'Hello';
+  let text2 = 'World!';
+  let text3 = 'World!';
+
+  let text4 = text1.concat(' ', text2, ' ', text3);
+  document.getElementById('demo-6').innerHTML = text4;
+}
+// console.log(text1); TODO: not find because of block scope {}
